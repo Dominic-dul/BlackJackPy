5b6c78eb5932fd9590b62eaf92670c5ce9db564c
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2022-09-05 20:49:57 +0300

    Create a Hand Class

--- blackjack.py
+++ blackjack.py
@@ -42,3 +42,19 @@ class Deck:
 # print(test_deck)
 # test_deck.shuffle()
 # print(test_deck)
+# print(test_deck.deal())
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
